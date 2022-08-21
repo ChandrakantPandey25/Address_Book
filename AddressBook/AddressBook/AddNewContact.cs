4abb1a58a75de94f79074a879d90a94bf6bdbc88
Chandrakant <chandrakant.pandey2014@yahoo.com>
2022-08-20 22:30:15 -0700

    Added new Contact

---
+++ AddressBook/AddressBook/AddNewContact.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AddressBook
+{
+    class AddNewContact
+    {
+        Dictionary<string, string> Contacts = new Dictionary<string, string>();
+        Dictionary<string, Dictionary<string, string>> AddressBook = new Dictionary<string, Dictionary<string, string>>();
+        public AddNewContact()
+        {
+            Console.WriteLine("Add the information required to enter the contact ");
+
+            Console.WriteLine("First Name");
+            Contacts.Add("First Name", Console.ReadLine());
+
+            Console.WriteLine("Last Name");
+            Contacts.Add("Last Name", Console.ReadLine());
+
+            Console.WriteLine("Address:");
+            Contacts.Add("Address", Console.ReadLine());
+
+            Console.WriteLine("City:");
+            Contacts.Add("City", Console.ReadLine());
+
+            Console.WriteLine("State:");
+            Contacts.Add("State", Console.ReadLine());
+
+            Console.WriteLine("Zip:");
+            int zip = Convert.ToInt32(Console.ReadLine());
+            Contacts.Add("Zip", Convert.ToString(zip));
+
+            Console.WriteLine("Phone number:");
+            int pNumber = Convert.ToInt32(Console.ReadLine());
+            Contacts.Add("Phone number", Convert.ToString(pNumber));
+
+            Console.WriteLine("Email:");
+            Contacts.Add("Email", Console.ReadLine());
+
+            Contacts.TryGetValue("First Name", out String FirstName);
+            Contacts.TryGetValue("Last Name", out String LastName);
+            AddressBook.Add(FirstName + " " + LastName, Contacts);
+            Console.WriteLine("contact added in the Address Book");
+        }
+    }
+}
--- AddressBook/AddressBook/Program.cs
+++ AddressBook/AddressBook/Program.cs
@@ -8,12 +8,16 @@ namespace AddressBook
         {
             Console.WriteLine("Welcome to Address Book Program!!");
             Console.WriteLine("Enter 1 to Create contact");
+            Console.WriteLine("Enter 2 to Add new contact");
 
             switch(Convert.ToInt32(Console.ReadLine()))
             {
                 case 1:
                     CreateContact contact = new CreateContact(/*First_Name,Last_Name,Address, City, State,  Zip, Phone_Number, email*/);
                     Console.WriteLine("Contact created");
+                    break;
+                case 2:
+                    AddNewContact newContact = new AddNewContact();
                     break;
                 default:
                     Console.WriteLine("Enter valid number!!");
